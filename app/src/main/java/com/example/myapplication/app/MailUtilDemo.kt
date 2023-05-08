dd850e2396cd35847a003a7642635452e68bf411
MRS <mikkelsrohde@hotmail.dk>
2023-05-08 10:17:34 +0200

    Email util and demo use

---
+++ app/src/main/java/com/example/myapplication/app/MailUtilDemo.kt
@@ -0,0 +1,355 @@
+package com.example.myapplication.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.MoveToInbox
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import com.example.myapplication.backend.Email
+import com.example.myapplication.backend.EmailUtil
+import kotlinx.coroutines.*
+import java.time.LocalDateTime
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(color = MaterialTheme.colors.background) {
+                SendEmailView()
+                //InboxView()
+                //TrashView()
+
+                /*SpamEmailsScreen(
+                    onEmailDeleted = {
+
+                    },
+                    onEmailMovedToInbox = {
+
+                    }
+                )*/
+            }
+        }
+    }
+
+
+    @Composable
+    fun InboxView() {
+
+        var emails by remember { mutableStateOf(listOf<Email>()) }
+
+        val context = LocalContext.current
+
+        LaunchedEffect(Unit){
+            try {
+                val mails = EmailUtil.fetchInbox("smtp.gmail.com", EmailUtil.mailFrom, EmailUtil.password)
+                emails = mails
+            }catch(e : Throwable){
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(context, "${e.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        LazyColumn {
+            items(emails) { email ->
+                Button(
+                    onClick = { /* Navigate to full email */ },
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.White,
+                        contentColor = Color.Black
+                    ),
+                    shape = RoundedCornerShape(16.dp),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 0.dp,
+                        pressedElevation = 8.dp
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                ) {
+                    Column {
+                        Text(
+                            text = "From: ${email.from}",
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "Subject: ${email.subject}",
+                            fontWeight = FontWeight.Normal
+                        )
+                        Text(
+                            text = "Received: ${email.receivedDate}",
+                            fontWeight = FontWeight.Normal
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun TrashView() {
+
+        var emails by remember { mutableStateOf(listOf<Email>()) }
+
+        var context = LocalContext.current
+
+        LaunchedEffect(Unit){
+            try {
+                val mails = EmailUtil.fetchTrashEmails("smtp.gmail.com", "587", EmailUtil.mailFrom, EmailUtil.password)
+                emails = mails
+            }catch (e : Throwable){
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(context, "${e.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        LazyColumn {
+            items(emails) { email ->
+                Button(
+                    onClick = { /* Navigate to full email */ },
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.White,
+                        contentColor = Color.Black
+                    ),
+                    shape = RoundedCornerShape(16.dp),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 0.dp,
+                        pressedElevation = 8.dp
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                ) {
+                    Column {
+                        Text(
+                            text = "From: ${email.from}",
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "Subject: ${email.subject}",
+                            fontWeight = FontWeight.Normal
+                        )
+                        Text(
+                            text = "Received: ${email.receivedDate}",
+                            fontWeight = FontWeight.Normal
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun SendEmailView() {
+        var to by remember { mutableStateOf("") }
+        var subject by remember { mutableStateOf("") }
+        var body by remember { mutableStateOf("") }
+
+        val context = LocalContext.current
+
+        Column(Modifier.padding(16.dp)) {
+            OutlinedTextField(
+                value = to,
+                onValueChange = { to = it },
+                label = { Text("To") }
+            )
+            OutlinedTextField(
+                value = subject,
+                onValueChange = { subject = it },
+                label = { Text("Subject") }
+            )
+            OutlinedTextField(
+                value = body,
+                onValueChange = { body = it },
+                label = { Text("Body") },
+                maxLines = 10
+            )
+            Button(
+                onClick = {
+                    val email = Email(
+                        id = "",
+                        from = EmailUtil.mailFrom,
+                        to = listOf(to),
+                        subject = subject,
+                        body = body,
+                        sentDate = LocalDateTime.now().toString(),
+                        receivedDate = "",
+                        isRead = false
+                    )
+
+                    CoroutineScope(Dispatchers.IO).launch {
+                        try {
+                            EmailUtil.sendEmail(
+                                host = "smtp.gmail.com",
+                                port = "587",
+                                username = EmailUtil.mailFrom,
+                                password = EmailUtil.password,
+                                mail = email
+                            )
+
+                            // Switch back to the main thread to show the toast
+                            withContext(Dispatchers.Main) {
+                                Toast.makeText(context, "Email sent", Toast.LENGTH_SHORT).show()
+                            }
+                        } catch (e: Exception) {
+                            // Handle any exceptions
+                            withContext(Dispatchers.Main) {
+                                Toast.makeText(context, "Failed to send email: ${e.message}", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                },
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                Text("Send")
+            }
+        }
+    }
+
+    @Composable
+    fun SpamEmailsScreen(onEmailDeleted: (Email) -> Unit, onEmailMovedToInbox: (Email) -> Unit) {
+
+        var spamMails by remember { mutableStateOf(listOf<Email>()) }
+
+        LaunchedEffect(Unit){
+            val mails = EmailUtil.fetchSpamEmails("smtp.gmail.com", "587", EmailUtil.mailFrom, EmailUtil.password)
+            spamMails = mails
+        }
+
+        val expandedMenuItemId = remember { mutableStateOf(-1) }
+        Column {
+            Text(
+                text = "Spam Emails",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                modifier = Modifier.padding(vertical = 16.dp, horizontal = 8.dp)
+            )
+            LazyColumn {
+                items(spamMails) { email ->
+                    val menuExpanded = expandedMenuItemId.value == email.hashCode()
+                    val options = listOf(
+                        EmailOption("Delete", Icons.Default.Delete) {
+                            onEmailDeleted(email)
+                        },
+                        EmailOption("Move to Inbox", Icons.Default.MoveToInbox) {
+                            onEmailMovedToInbox(email)
+                        }
+                    )
+                    EmailItem(
+                        email = email,
+                        onItemHold = {
+                            if (menuExpanded) {
+                                expandedMenuItemId.value = -1
+                            } else {
+                                expandedMenuItemId.value = email.hashCode()
+                            }
+                        },
+                        showOptionsMenu = menuExpanded,
+                        options = options
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun EmailItem(email: Email, onItemHold: () -> Unit, showOptionsMenu: Boolean = false, options: List<EmailOption> = emptyList()) {
+        val backgroundColor = if (email.isRead) Color.White else Color.LightGray
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp, horizontal = 8.dp)
+                .background(backgroundColor),
+            elevation = 4.dp
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = email.isRead,
+                    onCheckedChange = { },
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+                Text(
+                    text = email.from,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 8.dp)
+                )
+                Text(
+                    text = email.sentDate,
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = null,
+                    tint = Color.Gray,
+                    modifier = Modifier
+                        .padding(end = 8.dp)
+                        .clickable(onClick = onItemHold)
+                )
+            }
+            DropdownMenu(
+                expanded = showOptionsMenu,
+                onDismissRequest = { onItemHold() }
+            ) {
+                options.forEachIndexed { _, option ->
+                    DropdownMenuItem(
+                        onClick = {
+                            option.onClick()
+                            onItemHold()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = option.icon,
+                            contentDescription = null,
+                            tint = Color.Black,
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                        Text(
+                            text = option.text,
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    data class EmailOption(
+        val text: String,
+        val icon: ImageVector,
+        val onClick: () -> Unit
+    )
+
+
+}
+
+
+
--- app/src/main/java/com/example/myapplication/backend/EmailUtils.kt
+++ app/src/main/java/com/example/myapplication/backend/EmailUtils.kt
@@ -1,5 +1,8 @@
 package com.example.myapplication.backend
 
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import com.google.api.client.auth.oauth2.Credential
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
@@ -16,7 +19,10 @@ import jakarta.mail.*
 import jakarta.mail.Message.RecipientType.TO
 import jakarta.mail.internet.InternetAddress
 import jakarta.mail.internet.MimeMessage
+import jakarta.mail.search.FlagTerm
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.ByteArrayOutputStream
 import java.io.IOException
@@ -30,12 +36,14 @@ import org.apache.commons.codec.binary.Base64
 
 
 data class Email(
+    val id : String,
     val from: String,
     val to: List<String>,
     val subject: String,
     val body: String,
     val sentDate: String,
     val receivedDate: String,
+    val isRead : Boolean
 )
 
 object EmailUtil {
@@ -43,7 +51,16 @@ object EmailUtil {
     val mailFrom : String = "testemailappuser@gmail.com"
     val password : String = "ffabdfjtpgsqtlht"
 
-    fun sendEmail(host: String, port: String, username: String, password: String, mail: Email) {
+
+    /**
+     * Sends a mail from the specified account
+     * @param host
+     * @param port
+     * @param username
+     * @param password
+     * @param mail
+     */
+    suspend fun sendEmail(host: String, port: String, username: String, password: String, mail: Email) = withContext(Dispatchers.IO) {
         val properties = Properties().apply {
             put("mail.smtp.host", host)
             put("mail.smtp.port", port)
@@ -74,7 +91,14 @@ object EmailUtil {
         }
     }
 
-    suspend fun fetchEmails(host: String, username: String, password: String) : List<Email> = withContext(Dispatchers.IO){
+    /**
+     * Fetches the email from an smtp server
+     * @param host
+     * @param username
+     * @param password
+     * @return List<Email>
+     */
+    suspend fun fetchInbox(host: String, username: String, password: String) : List<Email> = withContext(Dispatchers.IO){
 
         val fetchedInbox : MutableList<Email> = mutableListOf()
 
@@ -87,10 +111,16 @@ object EmailUtil {
         store.connect(host, username, password)
 
         val inbox = store.getFolder("INBOX")
-        inbox.open(Folder.READ_ONLY)
 
-        val messages = inbox.getMessages(5, 10)
-        //val messages = inbox.messages
+        if (inbox.exists()) {
+            inbox.open(Folder.READ_ONLY)
+        } else {
+            throw Exception("Inbox is empty")
+        }
+
+
+        val messages = inbox.messages
+
         for (message in messages) {
 
             val recipients = message.getRecipients(TO)
@@ -102,17 +132,135 @@ object EmailUtil {
             }
 
             var tmpEmail = Email(
+                message.messageNumber.toString(),
                 message.from[0].toString(),
                 toList,
-                message.subject,
+                message.subject ?: "empty",
                 message.content.toString(),
                 "",
-                message.receivedDate.toString()
-                )
+                message.receivedDate.toString(),
+                message.flags.contains(Flags.Flag.SEEN)
+            )
 
             fetchedInbox.add(tmpEmail)
         }
         fetchedInbox
+    }
+
+    suspend fun fetchSpamEmails(host: String, port: String, username: String, password: String): List<Email> = withContext(Dispatchers.IO) {
+
+        val properties = Properties().apply {
+            put("mail.store.protocol", "imaps")
+        }
+
+        val session = Session.getInstance(properties, null)
+        val store = session.getStore("imaps")
+        store.connect(host, username, password)
+
+        val spam = store.getFolder("[Gmail]/Spam")
+        if (spam.exists()) {
+            spam.open(Folder.READ_ONLY)
+        } else {
+            throw Exception("Spam is empty")
+        }
+
+        val messages = spam.messages
+
+        val emails = mutableListOf<Email>()
+        messages.forEach { message ->
+            val id = message.messageNumber.toString()
+            val from = message.from?.get(0)?.toString() ?: ""
+            val to = (message.getRecipients(Message.RecipientType.TO) ?: arrayOf()).map { it.toString() }
+            val subject = message.subject ?: ""
+            val body = message.content?.toString() ?: ""
+            val sentDate = message.sentDate?.toString() ?: ""
+            val receivedDate = message.receivedDate?.toString() ?: ""
+            val isRead = message.isSet(Flags.Flag.SEEN)
+
+            val email = Email(id, from, to, subject, body, sentDate, receivedDate, isRead)
+            emails.add(email)
+        }
+
+        spam.close(false)
+        store.close()
+
+        emails.toList()
+    }
+
+    suspend fun fetchTrashEmails(host: String, port: String, username: String, password: String) : List<Email> = withContext(Dispatchers.IO){
+        val properties = Properties().apply {
+            put("mail.store.protocol", "imaps")
+        }
+
+        val session = Session.getInstance(properties, null)
+        val store = session.getStore("imaps")
+        store.connect(host, username, password)
+
+
+        val trashFolder = store.getFolder("[Gmail]/Papirkurv")
+        if (trashFolder.exists()) {
+            trashFolder.open(Folder.READ_ONLY)
+        } else {
+            throw Exception("Trash is empty")
+        }
+
+        val messages = trashFolder.messages
+
+        val emails = mutableListOf<Email>()
+        messages.forEach { message ->
+            val id = message.messageNumber.toString()
+            val from = message.from?.get(0)?.toString() ?: ""
+            val to = (message.getRecipients(Message.RecipientType.TO) ?: arrayOf()).map { it.toString() }
+            val subject = message.subject ?: ""
+            val body = message.content?.toString() ?: ""
+            val sentDate = message.sentDate?.toString() ?: ""
+            val receivedDate = message.receivedDate?.toString() ?: ""
+            val isRead = message.isSet(Flags.Flag.SEEN)
+
+            val email = Email(id, from, to, subject, body, sentDate, receivedDate, isRead)
+            emails.add(email)
+        }
+
+        trashFolder.close(false)
+        store.close()
+
+        emails.toList()
+    }
+
+    suspend fun deleteMailByID(host: String, username: String, password: String, messageNumber : String, mailList : MutableList<Email>) = withContext(Dispatchers.IO){
+        try {
+            val props = Properties()
+            props["mail.store.protocol"] = "imaps"
+            props["mail.imaps.host"] = host
+            props["mail.imaps.port"] = "993"
+            props["mail.imaps.starttls.enable"] = "true"
+
+            val session = Session.getDefaultInstance(props, null)
+            val store = session.getStore("imaps")
+            store.connect(host, username, password)
+
+            // Find the Trash folder
+            val trashFolder = store.getFolder("[Gmail]/Papirkurv")
+            if (!trashFolder.exists()) {
+                throw Exception("Trash doesn't exist")
+            }
+            trashFolder.open(Folder.READ_WRITE)
+
+            // Get the message by ID
+            val message = trashFolder.getMessage(messageNumber.toInt())
+
+            // Set the message's Deleted flag to true
+            message.setFlag(Flags.Flag.DELETED, true)
+
+            // Close the folder and expunge deleted messages to actually delete the email
+            trashFolder.close(true)
+
+            mailList.find{ it.id == messageNumber}?.let{ mailList.remove(it) }
+
+        } catch (e: Exception) {
+            // An exception occurred, return an error code or throw it
+            throw Exception("Unable to delete email. It might already be deleted")
+        }
     }
 
 }
